ea9e6e7730e94bb07a6de35c4136796a56335785
Arbab519 <bobetts786@gmail.com>
2023-03-15 11:42:49 +0500

    Dart Task 1

---
+++ Basic_Programing_1.dart
@@ -0,0 +1,50 @@
+// TASK 1
+void main() {
+  int first = 12;
+  int second = 2;
+  int third = 9;
+  if (first > second && first > third) {
+    print('$first Is Greatest');
+  } else if (second > first && second > third) {
+    print('$second Is Greatest');
+  } else {
+    print('$third Is Greatest');
+  }
+}
+// TASK 2
+// void main(){
+//   int firstNum=15;
+//   int secondNum=5;
+//   double ratio =firstNum/secondNum;
+//   print('ratio of $firstNum to $secondNum is $ratio');
+// }
+
+// Task 3
+// void main(){
+//   for(int i=1; i<=7; i++){
+//     int cubes= i*i*i;
+//     print ('$i Cube $cubes');
+//   }
+// }
+
+// Task 4
+// void main(){
+//   for(int i=1; i<=10; i++){
+//     int table=12*i;
+//     print('12 x $i = $table');
+//   }
+// }
+
+// void main(){
+//   int factorial=1;
+//   print('Enter Number :');
+//      int num = 5;
+//   if(num<1){
+//     print('Invalid Number');
+//   }else{
+//     for(int i=1; i<=num; i++){
+//       factorial *=i;
+//     }
+//     print('$factorial');
+//   }
+// }
